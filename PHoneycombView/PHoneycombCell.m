9abe25c81c865c79d4825698253018a3eac2eeb4
crazypoo <273277355@qq.com>
2015-06-19 17:20:18 +0800

    no message

--- PHoneycombView/PHoneycombCell.m
+++ PHoneycombView/PHoneycombCell.m
@@ -32,26 +32,36 @@
 
 -(void)drawRect:(CGRect)rect
 {
-#warning 还有bug,未能根据item大小来变化
     //// Polygon Drawing
-    UIBezierPath* polygonPath = UIBezierPath.bezierPath;
-    [polygonPath moveToPoint: CGPointMake(CGRectGetMinX(rect) + 50, CGRectGetMaxY(rect) + 0.9)];
-    [polygonPath addLineToPoint: CGPointMake(CGRectGetMinX(rect) + 93.3, CGRectGetMaxY(rect) - 20.85)];
-    [polygonPath addLineToPoint: CGPointMake(CGRectGetMinX(rect) + 93.3, CGRectGetMaxY(rect) - 64.35)];
-    [polygonPath addLineToPoint: CGPointMake(CGRectGetMinX(rect) + 50, CGRectGetMaxY(rect) - 86.1)];
-    [polygonPath addLineToPoint: CGPointMake(CGRectGetMinX(rect) + 6.7, CGRectGetMaxY(rect) - 64.35)];
-    [polygonPath addLineToPoint: CGPointMake(CGRectGetMinX(rect) + 6.7, CGRectGetMaxY(rect) - 20.85)];
-    [polygonPath closePath];
+//    UIBezierPath* polygonPath = UIBezierPath.bezierPath;
+//    [polygonPath moveToPoint: CGPointMake(CGRectGetMinX(rect) + 50, CGRectGetMaxY(rect) + 0.9)];
+//    [polygonPath addLineToPoint: CGPointMake(CGRectGetMinX(rect) + 93.3, CGRectGetMaxY(rect) - 20.85)];
+//    [polygonPath addLineToPoint: CGPointMake(CGRectGetMinX(rect) + 93.3, CGRectGetMaxY(rect) - 64.35)];
+//    [polygonPath addLineToPoint: CGPointMake(CGRectGetMinX(rect) + 50, CGRectGetMaxY(rect) - 86.1)];
+//    [polygonPath addLineToPoint: CGPointMake(CGRectGetMinX(rect) + 6.7, CGRectGetMaxY(rect) - 64.35)];
+//    [polygonPath addLineToPoint: CGPointMake(CGRectGetMinX(rect) + 6.7, CGRectGetMaxY(rect) - 20.85)];
+//    [polygonPath closePath];
+//    [UIColor.orangeColor setFill];
+//    [polygonPath fill];
+    CGFloat longSide = SIZE * 0.5 * cosf(M_PI * 30 / 180);
+    CGFloat shortSide = SIZE * 0.5 * sin(M_PI * 30 / 180);
+    UIBezierPath *path = [UIBezierPath bezierPath];
+    [path moveToPoint:CGPointMake(0, longSide)];
+    [path addLineToPoint:CGPointMake(shortSide, 0)];
+    [path addLineToPoint:CGPointMake(shortSide + SIZE * 0.5, 0)];
+    [path addLineToPoint:CGPointMake(SIZE, longSide)];
+    [path addLineToPoint:CGPointMake(shortSide + SIZE * 0.5, longSide * 2)];
+    [path addLineToPoint:CGPointMake(shortSide, longSide * 2)];
+    [path closePath];
     [UIColor.orangeColor setFill];
-    [polygonPath fill];
+    [path fill];
     
     CAShapeLayer *maskLayer = [CAShapeLayer layer];
-    maskLayer.path = [polygonPath CGPath];
+    maskLayer.path = [path CGPath];
     
     self.layer.mask = maskLayer;
     self.titleLabel.frame = self.contentView.frame;
     deleteBtn.frame = CGRectMake((self.contentView.frame.size.width-20)/2, 5, 20, 20);
-
 }
 
 #pragma mark -Delegate
